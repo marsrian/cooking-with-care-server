cb9b940b0885b2ba814a2c8eda31399cb31f82cb
marsrian <marsrian40@gmail.com>
2023-05-01 22:35:07 +0600

    express cors install done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef Recipe Hunter Server is ready...')
