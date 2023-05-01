c15e963be451cc5776027b376114dd1e7b1e9c16
marsrian <marsrian40@gmail.com>
2023-05-02 02:45:23 +0600

    json data file created

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef Recipe Hunter Server is ready...')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
 
 app.listen(port, () => {
