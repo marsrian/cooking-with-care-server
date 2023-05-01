a0f0e7b6e39d8977c9a5b566caf4f7a991069770
marsrian <marsrian40@gmail.com>
2023-05-02 03:24:07 +0600

    id find by chefs data done

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(c =>c._id === id);
+    res.send(selectedChef);
+})
+
 app.listen(port, () => {
     console.log('Server is ready: ', port);
 })
