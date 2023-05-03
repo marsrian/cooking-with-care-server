2ba22bdd77ba48f32641e90f7d38cf0a6cc9ef36
marsrian <marsrian40@gmail.com>
2023-05-03 13:21:33 +0600

    versel json file added

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const chefs = require('./data/chefs.json');
+const chefrecipes = require('./data/chefs.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef Recipe Hunter Server is ready...')
 })
 
-app.get('/chefs', (req, res) => {
-    res.send(chefs);
+app.get('/chefrecipes', (req, res) => {
+    res.send(chefrecipes);
 })
 
-app.get('/chefs/:id', (req, res) => {
+app.get('/chefrecipes/:id', (req, res) => {
     const id = req.params.id;
-    const selectedChef = chefs.find(c =>c._id === id);
+    const selectedChef = chefrecipes.find(c =>c._id === id);
     res.send(selectedChef);
 })
 
